788a5a4f68487d82f71b162713b27e9f0841322e
munirapp <softwaremakassar@gmail.com>
2025-05-09 10:54:38 +0700

    feat: add api middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
+import { verifyAuthToken } from "./lib/utils";
+
+const publicApiPaths = [
+  "/api/stats",
+  "/api/auth/login",
+  "/api/health",
+  "/api/auth/register",
+];
 
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // Check if the request is for an API route and not a public API path
+  if (pathname.startsWith("/api") && !publicApiPaths.includes(pathname)) {
+    const token = request.headers.get("Authorization");
+
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+      // Verify the JWT token
+      await verifyAuthToken(token.replace("Bearer ", ""));
+      return NextResponse.next();
+    } catch (error) {
+      console.error("JWT verification failed:", error);
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  }
+
   return NextResponse.next();
 }
